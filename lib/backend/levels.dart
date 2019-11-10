d6d761883e126346efba0b396d5b11802ca1e8f6
Ahnaf <ahnaf.24csedu.039@gmail.com>
2019-11-11 00:14:35 +0600

    added levels with a json file

---
+++ lib/backend/levels.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/services.dart';
+import 'dart:convert';
+import 'dart:ui';
+
+class Level {
+  Level({this.level, this.difficulty, this.frogs, this.toads, this.userFirst, this.leafs});
+  final int level;
+  final int difficulty;
+  final bool userFirst;
+  final int toads, frogs, leafs;
+
+  factory Level.fromMap(Map<String, dynamic> data) {
+    return Level(
+      difficulty: (data['diff'] == "easy") ? 1:2,
+      level: data['level'],
+      userFirst: (data['firstMove'] == "user") ? true : false,
+      frogs: data['frogs'],
+      toads: data['toads'],
+      leafs: data['leafs'],
+    );
+  }
+}
+
+class LevelData {
+  static List<Level> levels = List<Level>();
+  static Map<String, dynamic> data;
+  static List<Map<String, dynamic>> levelData = List<Map<String, dynamic>>();
+
+  LevelData() {
+    loadData();
+  }
+  Future loadJSON() async {
+    return await rootBundle.loadString('json/levels.json');
+  }
+
+  void loadData() async {
+    String jsonString = await loadJSON();
+    final jsonRespone = await json.decode(jsonString);
+    data = jsonRespone;
+    for (int i = 0; i < 20; i++) {
+      levelData.add(data['levels'][i]);
+    }
+    for (int i = 0; i < 20; i++) {
+      levels.add(Level.fromMap(levelData[i]));
+    }
+  }
+  
+}
